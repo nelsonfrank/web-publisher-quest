e405fba3cdf33a8483331f20f0cbe7df8172d6c1
nelsonfrank <nelsonfrank741@gmail.com>
2025-02-01 15:04:11 +0300

    fix: Improve logo and navigation item responsibility

--- src/components/layouts/main-nav.tsx
+++ src/components/layouts/main-nav.tsx
@@ -8,26 +8,29 @@ import { GlobeIcon } from "@radix-ui/react-icons";
 
 export function MainNav() {
   return (
-    <div className="hidden gap-6 w-full lg:flex lg:justify-between my-8">
-      <div className="flex gap-24">
-        <Link href="/" className="hidden items-center space-x-2 lg:flex">
-          <Icons.logo className="size-50" aria-hidden="true" />
+    <div className="hidden gap-6 w-full lg:flex lg:justify-between lg:items-center my-6">
+      <div className="flex justify-center items-center gap-20">
+        <Link href="/" className="hidden items-center lg:flex">
+          <Icons.logo
+            className="w-[300px] h-[29.4px] flex-grow-0 object-contain"
+            aria-hidden="true"
+          />
           <span className="sr-only">Home</span>
         </Link>
-        <ul className="flex gap-24">
-          <li>
+        <ul className="flex font-bold text-xl  leading-8 -tracking-[0.2px]">
+          <li className="w-[168px] text-center">
             <Link href="/">센터원</Link>
           </li>
-          <li>
+          <li className="w-[168px] text-center">
             <Link href="/">이용안내</Link>
           </li>
-          <li>
+          <li className="w-[168px] text-center">
             <Link href="/">서비스소개</Link>
           </li>
-          <li>
+          <li className="w-[168px] text-center">
             <Link href="/">임대문의</Link>
           </li>
-          <li>
+          <li className="w-[168px] text-center">
             <Link href="/">고객지원</Link>
           </li>
         </ul>
