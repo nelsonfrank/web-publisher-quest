af5580945e6b4bb887d17115e097324b87ac544a
nelsonfrank <nelsonfrank741@gmail.com>
2025-02-01 17:12:38 +0300

    fix: Make hero section pixel perfect

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -8,8 +8,8 @@ import { Icons } from "../icons";
 
 export function SiteHeader() {
   return (
-    <header className="relative w-full h-[75vh] bg-transparent">
-      <div className="relative top-0 z-50 w-full h-16 flex items-center container mx-auto">
+    <header className="relative w-full h-[1000px] bg-transparent">
+      <div className="relative top-0 z-50 w-full h-20 flex items-center container mx-auto">
         <MainNav />
         <MobileNav items={siteConfig.mainNav} />
       </div>
@@ -28,18 +28,22 @@ export function SiteHeader() {
 
         <div className="absolute inset-0 bg-black opacity-30"></div>
       </section>
-      <div className="relative z-50 h-full text-red flex  flex-col justify-end pb-36 mx-8 lg:pb-0 lg:justify-center lg:container lg:mx-auto">
-        <h1 className="text-6xl font-heading mb-8 flex flex-col text-white lg:text-9xl">
-          <span className="inline-flex mb-4 ">
-            THE <Icons.first className="ml-4 lg:ml-8 lg:size-32 lg:-mt-2" />
+      <div className="relative z-50 h-full text-red flex  flex-col justify-end pb-36 mx-8 lg:pb-0 lg:mt-[203px] lg:justify-start lg:container lg:mx-auto">
+        <h1 className="text-[180px] font-heading mb-8 flex flex-col text-white lg:text-9xl">
+          <span className="inline-flex mb-2 tracking-tight text-[143px] ">
+            THE{" "}
+            <Icons.first className="ml-4 lg:ml-[3.52rem] lg:w-[145px] lg:h-[145px] lg:-mt-3.5" />
           </span>
-          <span>BUILDING</span>
+          <span className="leading-[1.14] tracking-tight text-[143px]">
+            BUILDING
+          </span>
         </h1>
-        <div>
-          <p className="text-lg font-bold mb-2 lg:text-2xl">
-            최고의 공간을 경험하세요
+        <div className="lg:mt-6">
+          <p className=" font-bold mb-4 lg:text-[32px] lg:tracking-tight ">
+            <span className="">최고의</span> <span>공간을</span>{" "}
+            <span>경험하세요</span>
           </p>
-          <p className="font-light text-xs lg:text-lg">
+          <p className="font-light text-xl lg:ml-1.5 lg:text-lg  lg:tracking-wider ">
             국내를 넘어 세계의 무대를 준비하는 여러분들과 함께 합니다.
           </p>
         </div>
